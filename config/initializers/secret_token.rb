b31ad0e7612cca0687602408dcb36c88ff9f59f8
Daniel Heath <daniel.r.heath@gmail.com>
2013-06-30 21:31:59 +1000

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uploaderiffic::Application.config.secret_key_base = '47dab19cd4ca0a860f5c07a181b6750e147177db2f3749da5f0fdb403962a7a331fa7ef8efcc0463f7a81ed2fc38d7b9ba6faf94526797b4ca151041d947468c'
